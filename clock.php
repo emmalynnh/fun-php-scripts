c94aee018c7a9cdb8c888d580db09d5de06bfd9a
Emmalynn H <emmalynn.hoyt@protonmail.com>
2020-12-31 12:30:37 -0600

    Adding clock

---
+++ clock.php
@@ -0,0 +1,24 @@
+<? 
+
+//======================================================================
+// fun-php-scripts - PHP CLI CLOCK
+//======================================================================
+
+/*
+* Run from shell with the following command:
+*
+*   php date.php
+*
+*/
+
+function clock() {
+  for ($i = 0; ++$i;) {
+    system('clear');
+    echo date(("m/d/Y h:i:sa"));
+    sleep(1);
+  }
+}
+
+clock();
+?>
+
